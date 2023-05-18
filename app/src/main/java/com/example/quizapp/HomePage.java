db114fcb1869cc8004df1d3a0a01e434bee8c08f
Rafail-Vasilakis <dpsd18011@aegean.gr>
2023-05-18 14:49:25 +0300

    new_changes

--- app/src/main/java/com/example/quizapp/HomePage.java
+++ app/src/main/java/com/example/quizapp/HomePage.java
@@ -8,24 +8,32 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class home_page  extends AppCompatActivity{
+public class HomePage extends AppCompatActivity {
 
     Button movies, games, sports, history;
 
     Button exitBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+        Button btn = findViewById(R.id.movies_btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setBackgroundColor(Color.RED);
+                openActivity();
+            }
+
+        });
     }
 
-    public void onButtonClick(View view) {
-        Button button = (Button) view;
-        button.setBackgroundColor(Color.WHITE);
-
+    public void openActivity() {
         // Navigate to the desired layout page
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
-    }
+}
