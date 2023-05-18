5124840a0d0a5f25ab6e09b6155500ba2ad889f8
Rafail-Vasilakis <dpsd18011@aegean.gr>
2023-05-18 16:18:22 +0300

    new_change

--- app/src/main/java/com/example/quizapp/HomePage.java
+++ app/src/main/java/com/example/quizapp/HomePage.java
@@ -20,20 +20,61 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.home_page);
 
         Button btn = findViewById(R.id.movies_btn);
+        Button btn1 = findViewById(R.id.games_btn);
+        Button btn2 = findViewById(R.id.sports_btn);
+        Button btn3 = findViewById(R.id.history_btn);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn.setBackgroundColor(Color.RED);
-                openActivity();
+                btn.setBackgroundColor(Color.MAGENTA);
+                openMain();
             }
 
         });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn1.setBackgroundColor(Color.MAGENTA);
+                openGameQuiz();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn2.setBackgroundColor(Color.MAGENTA);
+                openSportsQuiz();
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn3.setBackgroundColor(Color.MAGENTA);
+                openHistoryQuiz();
+            }
+        });
     }
 
-    public void openActivity() {
+
+
+    public void openMain() {
         // Navigate to the desired layout page
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openGameQuiz() {
+        Intent intent = new Intent(this, GamesQuiz.class);
+        startActivity(intent);
+    }
+
+    public void openSportsQuiz() {
+        Intent intent = new Intent(this, SportsQuiz.class);
+        startActivity(intent);
+    }
+
+    public void openHistoryQuiz() {
+        Intent intent = new Intent(this, HistoryQuiz.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/quizapp/QuestionAnswer.java
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -24,65 +24,65 @@ public class QuestionAnswer {
     };
 
     public static String game_questions[] ={
-            "Which company owns the android?",
-            "Which one is not the programming language?",
-            "Which app has green colour in its logo?",
-            "Which company owns the Apple?"
+            "Which video game franchise features a character named Mario?",
+            "Which video game series is developed by Rockstar Games?",
+            "In the game 'Fortnite', which of the following is NOT a playable character?",
+            "Which video game console is produced by Sony?"
     };
 
     public static String game_choices[][] = {
-            {"Google","Apple","Nokia","Samsung"},
-            {"Java","Kotlin","Notepad","Python"},
-            {"Facebook","Whatsapp","Instagram","Youtube"},
-            {"Google","Apple","Nokia","Samsung"}
+            {"The Legend of Zelda","Super Mario","Halo","Grand Theft Auto"},
+            {"Assassin's Creed","Call of Duty","Grand Theft Auto","Minecraft"},
+            {"John Wick","Spider-Man","Iron Man","Thanos"},
+            {"Xbox Series X","Nintendo Switch","PlayStation 5","PC (Personal Computer)"}
     };
 
     public static String game_correctAnswers[] = {
-            "Google",
-            "Notepad",
-            "Whatsapp",
-            "Apple"
+            "Super Mario",
+            "Grand Theft Auto",
+            "Spider-Man",
+            "PlayStation 5"
     };
 
     public static String sport_questions[] ={
-            "Which company owns the android?",
-            "Which one is not the programming language?",
-            "Which app has green colour in its logo?",
-            "Which company owns the Apple?"
+            "Which player has the most goals in the history of the FIFA World Cup?",
+            "Which team has won the most Super Bowl championships in NFL history?",
+            "Who holds the record for the most home runs in a single Major League Baseball (MLB) season?",
+            "Which tennis player has won the most Grand Slam singles titles?"
     };
 
     public static String sport_choices[][] = {
-            {"Google","Apple","Nokia","Samsung"},
-            {"Java","Kotlin","Notepad","Python"},
-            {"Facebook","Whatsapp","Instagram","Youtube"},
-            {"Google","Apple","Nokia","Samsung"}
+            {"Cristiano Ronaldo","Lionel Messi","Pele","Diego Maradona"},
+            {"New England Patriots","Pittsburgh Steelers","Dallas Cowboys","San Francisco 49ers"},
+            {"Babe Ruth","Barry Bonds","Hank Aaron","Sammy Sosa"},
+            {"Rafael Nadal","Roger Federer","Serena Williams","Novak Djokovic"}
     };
 
     public static String sport_correctAnswers[] = {
-            "Google",
-            "Notepad",
-            "Whatsapp",
-            "Apple"
+            "Pele",
+            "New England Patriots",
+            "Barry Bonds",
+            "Roger Federer"
     };
 
     public static String history_questions[] ={
-            "Which company owns the android?",
-            "Which one is not the programming language?",
-            "Which app has green colour in its logo?",
-            "Which company owns the Apple?"
+            "Who was the first President of the United States?",
+            "Which famous scientist developed the theory of relativity?",
+            "Which ancient civilization built the Great Pyramids of Giza?",
+            "What year did World War II end?"
     };
 
     public static String history_choices[][] = {
-            {"Google","Apple","Nokia","Samsung"},
-            {"Java","Kotlin","Notepad","Python"},
-            {"Facebook","Whatsapp","Instagram","Youtube"},
-            {"Google","Apple","Nokia","Samsung"}
+            {"George Washington","Thomas Jefferson","Abraham Lincoln","John Adams"},
+            {"Isaac Newton","Albert Einstein","Galileo Galilei","Nikola Tesla"},
+            {"Ancient Greece","Ancient Rome","Ancient Egypt","Ancient China"},
+            {"1943","1945","1947","1950"}
     };
 
     public static String history_correctAnswers[] = {
-            "Google",
-            "Notepad",
-            "Whatsapp",
-            "Apple"
+            "George Washington",
+            "Albert Einstein",
+            "Ancient Egypt",
+            "1945"
     };
 }
